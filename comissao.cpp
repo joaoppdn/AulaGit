4fdf859ee0ced55e6ba7bdb73e860c007bfadfb8
unknown <joaoppn@senaicecap.com>
2026-08-17 11:05:35 -0300

    Criacao de projeto que calcula o valor de comissao

---
+++ comissao.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+	
+	string nome;
+	double valorVenda;
+	double taxaComissao = 0.05;
+	double comissao;
+	
+	
+	cout << "Digite o nome do Vendedor: " << endl;
+	cin >> nome;
+	
+	cout << "Digite o valor de venda: " << endl;
+	cin >> valorVenda;
+	
+	comissao = valorVenda * taxaComissao;
+	
+	cout <<"Vendedor: " << nome << endl;
+	cout <<"Comissao: "<< comissao << endl; 
+	
+	return 0;
+	
+}
